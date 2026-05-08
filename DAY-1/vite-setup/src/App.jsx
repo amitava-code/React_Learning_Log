58d31be9c81658e87b3aee051c5854d931e951a2
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-05-08 14:45:36 +0530

    feat(day-1): setup React with Vite and understand project structure

---
+++ DAY-1/vite-setup/src/App.jsx
@@ -0,0 +1,16 @@
+
+const App = () => {
+  return (
+    <>
+    <div id="dad">
+      <h1>hello</h1>
+      <h2>hello</h2>
+    </div>
+    <div id="chacha">
+      <h3>hello</h3>
+    </div>
+    </>
+  )
+}
+
+export default App
