671d7ca155c45f580e9c4466f9b9ef433bc9c2e8
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-05-08 13:19:28 +0530

    feat(day-1): learn import/export basics

---
+++ DAY-1/Import & export/app.js
@@ -0,0 +1,5 @@
+const arr = [10,20,30,40]
+
+export const user="amitava"
+
+export default arr
---
+++ DAY-1/Import & export/script.js
@@ -0,0 +1,8 @@
+// default export 
+import arr from "./app.js"
+
+//named export
+import {user} from './app.js'
+
+
+console.log(arr,user)
